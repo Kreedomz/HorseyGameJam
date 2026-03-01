20c4a876f3ba0fc988bae042c1b991ed8fa9815e
Kreedomz <147833340+Kreedomz@users.noreply.github.com>
2026-03-01 14:17:18 +0000

    Merged both branches and made them work together

--- Horsey/Assets/Scripts/MainMenuManager.cs
+++ Horsey/Assets/Scripts/MainMenuManager.cs
@@ -18,7 +18,7 @@ public class MainMenuManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             TogglePause();
         }
